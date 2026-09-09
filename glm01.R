5982130d21c37f0ce47240251a6d486b54b2dcf4
Greg Nishihara <gnishihara@gmail.com>
2026-09-09 14:47:23 +0900

    Update

--- glm01.R
+++ glm01.R
@@ -18,3 +18,12 @@ ggplot(irisdf) +
       color = Species
     )
   )
+
+###################################
+# 検討するモデル
+# 応答変数： Petal.Width
+# 説明変数： Petal.Length, Species
+# モデル： Petal.Width ~ Petal.Length + Species + Petal.Length:Species
+
+
+
