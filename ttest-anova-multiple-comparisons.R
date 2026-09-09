8cdab444ea49552466955b1a60614115cec96011
Greg Nishihara <gnishihara@gmail.com>
2026-09-09 14:39:22 +0900

    Update

--- ttest-anova-multiple-comparisons.R
+++ ttest-anova-multiple-comparisons.R
@@ -34,17 +34,21 @@ fullmodel = lm(Petal.Length ~ Species, data = irisdf)
 ggplot(irisdf) + geom_boxplot(aes(x = Species, y = Petal.Length))
 
 summary(fullmodel)
-
 #一元配置分散分析（または線形回帰モデル）の結果、アヤメの種（Species）は花弁の長さに対して極めて有意な主効果を示した (F(2, 147) = 1180, p < 0.001, R^2(adj) = 0.94)。
 
-# 多重比較
+anova(fullmodel)
 
+# 多重比較
 avg_predictions(fullmodel, by = "Species") 
 
 # ボンフェロニー 
 hypotheses(fullmodel,  ~ pairwise, multcomp = "bonferroni")
 # hypotheses(fullmodel,  ~ pairwise, multcomp = "holm")
 
+eout = emmeans(fullmodel, ~Species)
+contrast(eout, "pairwise", adj = "holm")
+
+###############################################################################
 # 二元配置分散分析
 tgdf = 
   ToothGrowth |> 
