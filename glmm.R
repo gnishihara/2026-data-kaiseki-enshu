3a857735876b50971cb4bb955a6960890a43e40e
Greg Nishihara <gnishihara@gmail.com>
2026-09-11 09:51:53 +0900

    Update

--- glmm.R
+++ glmm.R
@@ -12,22 +12,23 @@ library(emmeans)
 # 疑似データの生成
 
 # シードを設定する
-set.seed(2026) # 疑似乱数のシード (seed) を固定する
-
-# タンク（水槽）　ランダム効果 (Random Effect)
+set.seed(2026) # 疑似乱数のシード (seed) を固定する# タンク（水槽）　ランダム効果 (Random Effect)
 # 合計 12 タンク: 6 コントロール　６ 高水温
 
+tanks_treatment = 12
+number_tanks = tanks_treatment * 2
+
 tanks = tibble(
-  tank = str_c("tank_", str_pad(1:12, 2, pad = "0")),
-  treatment = rep(c("control", "high_temp"), each = 6),
-  tank_effect = rnorm(12, mean = 0, sd = 0.8)
+  tank = str_c("tank_", str_pad(1:number_tanks, 2, pad = "0")),
+  treatment = rep(c("control", "high_temp"), each = tanks_treatment),
+  tank_effect = rnorm(number_tanks, mean = 0, sd = 0.8)
 )
 tanks
-ggplot(tanks) + geom_point(aes(x = treatment, y = tank_effect), position = position_jitter(width = 0.2))
+# ggplot(tanks) + geom_point(aes(x = treatment, y = tank_effect), position = position_jitter(width = 0.2))
 
 seaweeddf = 
   tanks |> 
-  uncount(30) |> 
+  uncount(1000) |> 
   mutate(
     shubyoid = row_number(),
     fixed_effect = if_else(str_detect(treatment, "Con"), 1, -0.5),
@@ -63,5 +64,7 @@ contrast(eout, "pairwise")
 
 # GLM:   survival = treatment + error
 # GLMM:  survival = (treatment + tank_variation) + error
- 
+# 各モデルの残渣デビアス (residual deviance)
+deviance(m01) # GLM residual deviance
+deviance(g01) # GLMM residual deviance
 
